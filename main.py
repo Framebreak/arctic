90ae4e15378139d1edb416ec604f7426af9d6cf1
Framebreak <jacq.coesfeld@gmail.com>
2022-01-30 18:49:17 +0100

    tests with methods/definitions tried to mak a method that would roll out stats for a dnd char

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randrange
+
+
 def Hello_World(irgentwas:str, seperator:chr):
     x = irgentwas.split(seperator)
     print(x)
@@ -9,6 +12,23 @@ def Flicken(input:list, seperator:chr):
         o = o + str(input[i]) + seperator
     o+= input[len(input)-1]
     return o
+
+def DndStats():
+    n = 0
+    while n <6:
+        firstStat = [0,0,0,0]
+        i = 0
+        while i < 4:
+            firstStat[i] = randrange(1, 6)
+
+            i+= 1
+        stat = (sorted(firstStat, reverse=True))
+        print(stat)
+        result = stat[0] + stat[1] + stat[2]
+        print(result)
+        n+=1
+
 liste = Hello_World('ach so geht das', "a")
 f = Flicken(liste, "a")
 print(f)
+DndStats()
